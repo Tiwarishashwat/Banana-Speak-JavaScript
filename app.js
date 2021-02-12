e0b6fe1c702e3a9f276aa5718e901e90079f4086
shashwat <shashwattiwari8@gmail.com>
2021-02-12 05:55:04 -0800

    done with inputs!

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 // var username=prompt("Give me a username");
 // var message="This script works "+username+"!";
 // alert(message)
-var btnTranslate = document.querySelector(this.#btn-translate);
-//Document.getElementById(btn-translate)
+// var btnTranslate = document.getElementById(btn-translate);
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+function clickEventHandler()
+{
+    console.log("clicked!");
+    console.log("input",txtInput.value);
+}
+btnTranslate.addEventListener("click",clickEventHandler);
 console.log(btnTranslate);
