7008496162dd4f24b6aaa1b2f07ae03846941451
shashwat <shashwattiwari8@gmail.com>
2021-02-12 04:22:16 -0800

    second commit

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-console.log("Hello Script is working!");
-var username=prompt("Give me a username");
-var message="This script works "+username+"!";
-alert(message)
+// var username=prompt("Give me a username");
+// var message="This script works "+username+"!";
+// alert(message)
+var btnTranslate = document.querySelector(this.#btn-translate);
+//Document.getElementById(btn-translate)
+console.log(btnTranslate);
