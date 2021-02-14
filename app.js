2805810c501d1beb6576ca440bdc4d85afb26ddb
shashwat <shashwattiwari8@gmail.com>
2021-02-14 14:37:01 +0530

    Created!

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 //Global Variables for input
-var btnTranslate = document.querySelector("#btn-translate");
-var txtInput = document.querySelector("#txt-input");
-var outputDiv = document.querySelector("#output");
-var wordLength = document.querySelector("#twitterConcept");
+var btnTranslate = document.getElementById("btn-translate");
+var txtInput = document.getElementById("txt-input");
+var outputDiv = document.getElementById("output");
+var wordLength = document.getElementById("twitterConcept");
 var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
 //Listening to events : Characters Left Function
 txtInput.addEventListener("keyup", function(){
     var counter=txtInput.value.length;
     var output = "You have written " + counter + " characters, and " + " you have " + (280 - counter) + " characters left!";
+    if(counter>280)
+    {
+        var st=txtInput.value.slice(0,280);
+        txtInput.innerText=st;
+    }
     wordLength.innerText = output;
 });
 
@@ -28,9 +33,8 @@ function clickEventHandler() {
     var inputText = txtInput.value;
     fetch(getTranslationURL(inputText))
         .then(response => response.json())
-        .then(json => outputDiv.innerText = "Translated: " + json.contents.translated)
+        .then(json => outputDiv.innerText = json.contents.translated)
         .catch(clickEventHandler);
 }
 //Listening to events : Translate Button Click
 btnTranslate.addEventListener("click", clickEventHandler);
-//console.log(btnTranslate);
