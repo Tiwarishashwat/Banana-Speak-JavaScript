62805bc9fa8c8b8bde9bed18cbf409c28eb3f539
shashwat <shashwattiwari8@gmail.com>
2021-02-14 12:44:50 +0530

    added twitter features!

--- app.js
+++ app.js
@@ -5,8 +5,15 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
-
+var wordLength = document.querySelector("#twitterConcept");
 var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function numberOfWords(text)
+{
+ wordLength.innerText="You have written "+txtInput.length+" characters,"+" you have "+(280-textInput.length)+" characters left!"
+}
+
+wordLength.addEventListener("keypress",numberOfWords)
 
 function getTranslationURL(text) {
     return serverURL + "?" + "text=" + text;
@@ -28,3 +35,4 @@ function clickEventHandler() {
 }
 btnTranslate.addEventListener("click", clickEventHandler);
 console.log(btnTranslate);
+//funtranslation
